2fc08dbe0fb030b3d21b382aabb241c031fa3d57
paper7grid <muddassirsadia123@gmail.com>
2025-08-17 12:24:39 -0700

    filename

--- Expense_tr.py
+++ Expense_tr.py
@@ -9,4 +9,15 @@ init(autoreset=True)
 
 #art banner
 banner = pyfiglet.figlet_format("Expense Tracker")
+print(Fore.CYAN + banner)
+print(Fore.YELLOW + "Welcome to the Expense Tracker!")
+
+FILE_NAME = "expenses.csv"
+if not os.path.exists(FILE_NAME):
+    with open(FILE_NAME, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date", "Category", "Description", "Amount"])
+
+def add_expense():
+    date = 
  
