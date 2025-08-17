19bdc06d049599ecee6c8afaf6f0cdfae0733f62
paper7grid <muddassirsadia123@gmail.com>
2025-08-17 11:15:44 -0700

    banner

--- Expense_tr.py
+++ Expense_tr.py
@@ -1,2 +1,12 @@
 from colorama import Fore, Style, init
+import pyfiglet
+import csv
 import os
+from datetime import datetime
+
+# Initialize colorama
+init(autoreset=True)
+
+#art banner
+banner = pyfiglet.figlet_format("Expense Tracker")
+ 
